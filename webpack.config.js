7f47602f34852707c455e9d00d403d8d8ddedefc
Kevin Lowe <iamkevinlowe@gmail.com>
2021-01-26 22:08:00 -0800

    Split popup files; More automation; Pause automation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = env => {
 	return {
 		mode: env.NODE_ENV || 'production',
 		entry: {
 			background: './src/background.js',
-			popup: './src/popup.js',
+			popup: ['./src/popup.js', '/src/popup.less'],
 			sd_covid: './src/sd_covid.js',
 			calvax: './src/calvax.js'
 		},
@@ -15,6 +16,14 @@ module.exports = env => {
 					test: /\.js$/,
 					exclude: /node_modules/,
 					use: ['babel-loader']
+				},
+				{
+					test: /\.less$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+						'less-loader'
+					]
 				}
 			]
 		},
@@ -23,7 +32,8 @@ module.exports = env => {
 				chunks: ['popup'],
 				filename: 'popup.html',
 				template: './src/popup.html'
-			})
+			}),
+			new MiniCssExtractPlugin()
 		],
 		devtool: 'source-map'
 	};
